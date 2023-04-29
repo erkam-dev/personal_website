64a096ff618ced7284013ec9eb1414eca1e3b995
“erkam-dev” <erkamfb14@gmail.com>
2023-04-29 15:37:45 +0300

    fix: injection container started at main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:personal_website/core/core.dart';
 
 import 'app/app_config.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  init();
   runApp(const App());
 }
